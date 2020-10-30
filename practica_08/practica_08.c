72b284bc4b3db65d2c9119b5307c255406c57e7d
Matias <matiasnoriega86@gmail.com>
2020-10-30 18:25:47 -0300

    antes de refactorizar con semaforos system v

--- practica_08/practica_08.c
+++ practica_08/practica_08.c
@@ -8,20 +8,50 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <semaphore.h>
+#include <sys/sem.h>
+
+int semid;
+
+void reservar(int semaforo){
+	struct sembuf buf;
+	buf.sem_num = semaforo;
+	buf.sem_op = -1;
+	buf.sem_flg = 0;
+
+	semop(semid, &buf, 1);
+}
+
+void liberar(int semaforo){
+	struct sembuf buf;
+	buf.sem_num = semaforo;
+	buf.sem_op = 1;
+	buf.sem_flg = 0;
+
+	semop(semid, &buf, 1);
+}
 
 int main(int argc, char *argv[]){
 	
-	pid_t pid = 1;
-	sem_t sem_compartido;
+	pid_t pid = getpid();
+	int cantidad_procesos_hijos = argc - 2;
 	char caracter_leido;
+	void *memoria_compartida = (void *)0;
+	int estado, shmid;
 	int *contador_compartido;
-	int estado, shmid, fd, contador_local;
-	
-	/* Creamos el semaforo sin nombre compartido y lo inicializamos en 1 (abierto) */
-	if(sem_init(&sem_compartido, 1, 1) < 0){
+	/* Creamos el semaforo sin nombre compartido y lo inicializamos en 1 (abierto)
+	if(sem_init(&sem_compartido, 1, 0) < 0){
 		perror("creacion semaforo");
 		exit(EXIT_FAILURE);
+	}*/
+
+	if((semid = semget(IPC_PRIVATE, cantidad_procesos_hijos, 0600)) < 0){
+		perror("semget");
+		exit(EXIT_FAILURE);
+	}
+	/*Inicializamos los semaforos, el primero en 1 y el resto en 0*/
+	semctl(semid, 0, SETVAL, 1);
+	for(int i = 1; i < cantidad_procesos_hijos; i++){
+		semctl(semid, i, SETVAL, 0);
 	}
 	
 	/* Creamos el segmento de memoria compartida */	
@@ -30,7 +60,8 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	}
 	printf("ARGC: %d\n", argc);	
-	contador_compartido = shmat(shmid, 0, 0);
+	memoria_compartida = shmat(shmid, 0, 0);
+	contador_compartido = (int *)memoria_compartida;
 	*contador_compartido = 0;
 	system("ipcs -sm");
 	
@@ -38,7 +69,7 @@ int main(int argc, char *argv[]){
 	 * La iteración empieza en k = 2 porque la posición 0 es el nombre del progra,a y la posición
 	 * 1 es la letra a comparar
 	*/
-	for(int k = 2; k < argc; k++){
+	for(int k = 0; k < cantidad_procesos_hijos; k++){
 		/* Chequeamos al inicio de cada iteración que sea solamente
 		 * el proceso padre el que cree los forks
 		 */
@@ -51,22 +82,22 @@ int main(int argc, char *argv[]){
 				
 			if(pid == 0){
 				printf("Hijo\n");
-				int valor;
-				sem_getvalue(&sem_compartido, &valor);
-				printf("sem compartido %d\n", valor);
+				int valor,fd;
+				/*sem_getvalue(&sem_compartido, &valor);
+				printf("sem compartido %d\n", valor);*/
 				/* Proceso Hijo: Se conecta al sector de memoria compartida */
-				if((contador_compartido = shmat(shmid, 0, 0)) < (int *)0){
+				/*if((contador_compartido = shmat(shmid, 0, 0)) < (int *)0){
 					perror("shmat");
 					exit(EXIT_FAILURE);
-				}
-				printf("contador compartido %d\n", *contador_compartido);	
+				}*/
+
 				/*if((contador_local = malloc(sizeof(int))) == NULL){
 					perror("malloc");
 					exit(EXIT_FAILURE);
 				}*/
 
 				/* Los procesos hijos van abriendo los archivos */
-				if((fd = open(argv[k], O_RDONLY)) < 0){
+				if((fd = open(argv[k+2], O_RDONLY)) < 0){
 					perror("open");
 					exit(EXIT_FAILURE);
 				}
@@ -74,24 +105,37 @@ int main(int argc, char *argv[]){
 				 * se paso por la linea de comandos, en caso de que se encuentre
 				 * uno se accede a la sección crítica utilizando el semáforo
 				 * compartido y aumentamos el contador en 1 */	
-				while(read(fd,&caracter_leido,sizeof(char))){
-					printf("caracter leido %c\n",caracter_leido);
-					if(strcmp(argv[1], &caracter_leido) == 0){
-						if(sem_wait(&sem_compartido) < 0){
+				while(read(fd,&caracter_leido,1)){
+					printf("caracter leido %c\n",(char)caracter_leido);
+
+					if(strncmp(argv[1], &caracter_leido,1) == 0){
+						/*
+						if(sem_wait(&sem_compartido) == -1){
 							perror("sem_wait");
 							exit(EXIT_FAILURE);
 						}
-	
-						contador_local = *contador_compartido;
-						contador_local++;
-						printf("contador local %d\n",contador_local);
-						*contador_compartido = contador_local;
+							
+						*contador_compartido = *contador_compartido + 1;
 
-						if(sem_post(&sem_compartido) < 0){
+						printf("contador compartido %d\n", *contador_compartido);
+						if(sem_post(&sem_compartido) == -1){
 							perror("sem_post");
 							exit(EXIT_FAILURE);
-						}
+						}*/
+						//printf("antes de reservar\n");
+						reservar(k);
+						//printf("despues de reservar\n");
 
+						*contador_compartido = *contador_compartido + 1;
+						
+						if((k+1) < cantidad_procesos_hijos){
+							//printf("se libera k+1: %d\n", k+1);
+							liberar(k+1);
+						}else{
+							//printf("no se libera k+1\n: %d", k+1);
+							liberar(k);
+						}
+						//printf("aca llega despues de liberar\n");
 					}
 				}
 				/* Cerramos el archivo */
@@ -99,27 +143,36 @@ int main(int argc, char *argv[]){
 					perror("close");
 					exit(EXIT_FAILURE);
 				}		
+
+				/* Hacemos que todos los procesos al llegar a este punto se de-attacheen de la memoria compartida */
+				if(shmdt(contador_compartido) < 0){
+					perror("shmdt");
+					exit(EXIT_FAILURE);
+				}
 			}	
 		}
 	}
-	/* Hacemos que todos los procesos al llegar a este punto se de-attacheen de la memoria compartida */
-	if(shmdt(contador_compartido) < 0){
-		perror("shmdt");
-		exit(EXIT_FAILURE);
-	}
+
 	/* El proceso padre espera a que finalicen los hijos y elimina el Semáforo y la Memoria Compartida*/
 	if(pid != 0){
-		while(wait(&estado) < 0);
+		//sem_post(&sem_compartido);		
+		while(wait(&estado) > 0);
 		printf("Se encontraron %d ocurrencias del caracter %s\n", *contador_compartido, argv[1]);
 		printf("Eliminamos el semáforo y la memoria compartida\n");
+		shmdt(memoria_compartida);
 		if(shmctl(shmid, IPC_RMID, 0) < 0){
 			perror("shmctl");
 			exit(EXIT_FAILURE);
 		}
+
+		if(semctl(semid, 0, IPC_RMID) < 0){
+			perror("sem_destroy");
+			exit(EXIT_FAILURE);
+		}/*
 		if(sem_destroy(&sem_compartido)){
 			perror("sem_destroy");
 			exit(EXIT_FAILURE);
-		}
+		}*/
 
 		system("ipcs -ms");
 
